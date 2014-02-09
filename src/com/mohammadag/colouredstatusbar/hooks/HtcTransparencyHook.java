4857990ad80c46601b82f24614b610217caa9a73
Vincent D <ogmniluje@gmail.com>
2014-02-09 11:55:47 +0100

    Fix StatusBarColor not applied on activity Resume on HTC devices

--- src/com/mohammadag/colouredstatusbar/hooks/HtcTransparencyHook.java
+++ src/com/mohammadag/colouredstatusbar/hooks/HtcTransparencyHook.java
@@ -1,14 +1,48 @@
 package com.mohammadag.colouredstatusbar.hooks;
 
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class HtcTransparencyHook {
 	public static void doHook(ClassLoader classLoader) {
 		if (!android.os.Build.MANUFACTURER.toLowerCase().contains("htc"))
 			return;
 
-		XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", classLoader,
-				"setStatusBarBackground", int.class, XC_MethodReplacement.DO_NOTHING);
+		findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", classLoader,
+                "setStatusBarBackground", int.class, XC_MethodReplacement.DO_NOTHING);
+
+        try {
+            Class<?> phoneTransitions = findClass("com.android.systemui.statusbar.phone.PhoneStatusBarTransitions", classLoader);
+
+            findAndHookMethod(phoneTransitions, "transitionTo", int.class, boolean.class, new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                    try {
+                        int lastMode = XposedHelpers.getIntField(param.thisObject, "lastMode");
+                        int newValue = (Integer) param.args[0];
+
+                        // Do not call setupBackground method when leaving the homescreen
+                        if(newValue != lastMode && newValue != 0){
+                            XposedHelpers.callMethod(param.thisObject, "setupBackground", newValue);
+                        }
+
+                        // Save new value to lastMode
+                        XposedHelpers.setIntField(param.thisObject, "lastMode", newValue);
+                    }
+                    catch (Exception ex)
+                    {
+                        XposedBridge.log("Error while mapping the new method 'transitionTo'");
+                    }
+                    return null;
+                }
+            });
+        }
+        catch(Exception ex){
+            XposedBridge.log("Can't hook PhoneStatusBarTransitions");
+        }
 	}
 }
